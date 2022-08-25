a48440c1b6cfaca7cf06f5df9d8f63dd4d81a95c
Marcus Bronson <71241055+mrbronny@users.noreply.github.com>
2022-08-25 20:37:04 +0000

    Initial commit

---
+++ textinfo.py
@@ -0,0 +1,39 @@
+
+with open("gettysburg.txt", "r") as file:
+  file = file.read()
+  characters = 0
+  uppers = 0
+  lowers = 0
+  digits = 0
+  whitespace = 0
+  vowels = {"a":0,"e":0,"i":0,"o":0,"u":0}
+  consonants = 0
+  words = 0
+  wordsPerSent = []
+  for char in file:
+    if char.isupper():
+      uppers += 1
+    if char.islower():
+      lowers += 1
+    if char.isdigit():
+      digits += 1
+    elif char in [" ","\n"]:
+      whitespace += 1
+      words += 1
+    elif char.lower() in vowels:
+      vowels[char.lower()] += 1
+    elif char.isalpha():
+      consonants += 1
+    elif char in [".","!","?"]:
+      wordsPerSent += [words]
+      words = 0
+    characters += 1
+  print(f"Characters: {characters}")
+  print(f"Upper case: {uppers}")
+  print(f"Lower case: {lowers}")
+  print(f"Digits: {digits}")
+  print(f"Whitespace: {whitespace}")
+  print(f"Vowels: {vowels}")
+  print(f"Consonants: {consonants}")
+  print(f"Sentences: {len(wordsPerSent)}")
+  print(f"Average words per sentence: {sum(wordsPerSent)/len(wordsPerSent)}")
